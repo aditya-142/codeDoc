3ef962bbafe35d6df126865ba4ffeafbc0e28aa5
aditya-142 <105287623+aditya-142@users.noreply.github.com>
2025-01-08 10:58:22 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -6,17 +6,17 @@ from llama_index.tools.code_interpreter.base import CodeInterpreterToolSpec
 import streamlit as st
 
 def return_agent():
-    # Azure OpenAI configuration
+    # API config
     openai.api_type = "azure"
-    openai.api_base = st.secrets['AZURE_API_BASE']  # Add this to Streamlit secrets
-    openai.api_version = "2023-05-15"  # Use the correct version for your Azure OpenAI resource
-    openai.api_key = st.secrets['AZURE_API_KEY']  # Add your API key to Streamlit secrets
+    openai.api_base = st.secrets['AZURE_API_BASE']  
+    openai.api_version = st.secrets['AZURE_OPENAI_API_VERSION']  
+    openai.api_key = st.secrets['AZURE_API_KEY']  
 
     code_spec = CodeInterpreterToolSpec()
     tools = code_spec.to_tool_list()
     agent = OpenAIAgent.from_tools(
         tools,
-        llm=OpenAI(temperature=0, model="gpt-4"),  # Replace "gpt-4" with your Azure deployment name
+        llm=OpenAI(temperature=0, model="gpt-4o"),  
         verbose=True
     )
     return agent
